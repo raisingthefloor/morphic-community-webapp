782bd1dc13eee07dbd01bfb75bbf7137f0685fde
ste <git@grundell.co.uk>
2021-05-17 13:55:24 +0100

    Added "snip" button

--- src/utils/allButtons.js
+++ src/utils/allButtons.js
@@ -975,6 +975,23 @@ export const allButtons = {
             url: "https://open.spotify.com/"
         }
     },
+    snip: {
+        kind: "action",
+        is_primary: true,
+        data: {
+            visual: {
+                type: "",
+                buttons: ""
+            }
+        },
+        configuration: {
+            subkind: "action",
+            label: "Snip",
+            description: "Copy a region of your screen to the clipboard",
+            identifier: "snip",
+            color: "blue"
+        }
+    },
     target: {
         kind: "link",
         is_primary: false,
