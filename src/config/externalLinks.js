7fbd8c238f606ed80d2ea2c15cdc9e4fe8912fc4
ste <git@grundell.co.uk>
2021-08-31 12:07:08 +0100

    Removed redundant link from the external links list

--- src/config/externalLinks.js
+++ src/config/externalLinks.js
@@ -4,7 +4,6 @@ export default {
     privacyPolicy: "https://morphic.org/privacy-policy/",
     termsOfUse: "https://morphic.org/terms-of-service/",
     earlyReleaseProgram: "https://morphic.org/notify/",
-    gettingStarted: "https://i716wo.axshare.com/#c=2",
     // Email link, Learn more about morphic
     learnMore: "https://morphic.org",
     // Email link, for managers to help users get setup.
